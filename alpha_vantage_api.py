6fd482fd2cb584b97b8f93be7a3e91af3aba73b5
Gregory Eales <gregoeales@aol.com>
2019-03-10 15:06:09 -0700

    intraday method complete

--- alpha_vantage_api.py
+++ alpha_vantage_api.py
@@ -17,16 +17,16 @@ class AlphaVantageAPI(object):
         self._link_interval = "&interval="
         self._link_key = "&apikey="
         self._link_dtype = "&datatype="
+        self._link_output = "&outputsize="
 
     def fetch_intraday_example(self):
         df = pd.read_csv(self._example_link)
         return df
 
-    def _create_link(self, func, ticker, interval, dtype):
-        self._check_func(func)
+    def _create_link(self, func, ticker, interval, output_size="compact", dtype="csv"):
         self._check_interval(interval)
-        link = self._link_main + func + self._link_symbol + ticker + self._link_interval + \
-               interval + self._link_key + self._api_key + self._link_dtype + dtype
+        link = self._link_main + func + self._link_symbol + ticker + self._link_interval + interval + \
+               self._link_output + output_size + self._link_key + self._api_key + self._link_dtype + dtype
         return link
 
     @staticmethod
@@ -35,21 +35,30 @@ class AlphaVantageAPI(object):
         if interval not in intervals:
             raise Exception("Error: the interval entered is not valid")
 
-    @staticmethod
-    def _check_func(self, func):
-        pass
+    def fetch_intraday(self, ticker, interval, output_size="compact", dtype="csv"):
+        func = "TIME_SERIES_INTRADAY"
+        self._check_interval(interval)
 
-    def fetch_intraday(self):
-        pass
+        link = self._create_link(func, ticker, interval, output_size, dtype)
+        df = pd.read_csv(link)
+        return df
 
     def fetch_daily(self):
-        pass
+
+        df = pd.read_csv(link)
+        return df
 
     def fetch_weekly(self):
-        pass
+
+        df = pd.read_csv(link)
+        return df
 
     def fetch_monthly(self):
-        pass
+
+        df = pd.read_csv(link)
+        return df
 
     def fetch_quote_endpoint(self):
-        pass
+
+        df = pd.read_csv(link)
+        return df
--- main.py
+++ main.py
@@ -6,6 +6,6 @@ my_api_key = "1HWD90PW0GC2JH38"
 fetcher = AlphaVantageAPI(api_key=my_api_key)
 
 
-df = fetcher.fetch_example()
+df = fetcher.fetch_intraday("MSFT", "1min", )
 
 print(df.head(1))
