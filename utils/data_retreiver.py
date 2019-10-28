c9dc9d99a49215f928a64c91f3ab7bc05908214e
Gregory Eales <gregoeales@aol.com>
2019-10-28 03:07:17 -0700

    Create data_retreiver.py

---
+++ utils/data_retreiver.py
@@ -0,0 +1,12 @@
+import numpy as np
+import torch
+
+import selenium
+from BeautifulSoup import BeautifulSoup
+import pandas as pd
+
+
+class DataRetreiver(object):
+
+    def __init__(self):
+        pass
