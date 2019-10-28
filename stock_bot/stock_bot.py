1fffccd0621fb2109d820edc80719a20c6b9ca61
Gregory Eales <gregoeales@aol.com>
2019-10-28 01:34:24 -0700

    Update stock_bot.py

--- stock_bot/stock_bot.py
+++ stock_bot/stock_bot.py
@@ -1,10 +1,34 @@
 import torch
-import etrade_python_client
-
+import pyetrade
+from pyetrade import ETradeAccessManager, ETradeAccounts, ETradeMarket
+from pyetrade import ETradeOAuth, ETradeOrder
+from pyetrade import
 
 class StockBot(object):
 
     def __init__(self):
+
+        self.client_key = None
+        self.client_secret = None
+        self.resource_owner_key = None
+        self.resource_owner_secret = None
+
+        self.access_manager = ETradeAccessManager(client_key=,
+         client_secret=, resource_owner_key=, resource_owner_secret=)
+
+        self.acounts = ETradeAccounts(client_key=,
+         client_secret=, resource_owner_key=, resource_owner_secret=)
+
+        self.market = ETradeMarket(client_key=, client_secret=,
+         resource_owner_key=, resource_owner_secret=)
+
+        self.oauth = ETradeOAuth(consumer_key=,
+         consumer_secret=, callback_url='oob')
+
+    def get_credentials(self, filename):
+        pass
+
+    def login(self):
         pass
 
     def buy(self, ticker, amount):
@@ -14,4 +38,7 @@ class StockBot(object):
         pass
 
     def get_data(self, ticker, amount):
+        pass
+
+    def trade(self):
         pass
