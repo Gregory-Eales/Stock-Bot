8be817470ca95c127dc84980803d09fcbfd3e4bd
Gregory Eales <gregoeales@aol.com>
2019-10-28 00:28:46 -0700

    Update stock_bot.py

--- stock_bot/stock_bot.py
+++ stock_bot/stock_bot.py
@@ -1,5 +1,17 @@
+import torch
+import etrade_python_client
+
 
 class StockBot(object):
 
     def __init__(self):
+        pass
+
+    def buy(self, ticker, amount):
+        pass
+
+    def sell(self, ticker, amount):
+        pass
+
+    def get_data(self, ticker, amount):
         pass
