fc07493ebf3cab89523655f1e49d30a0b46fed57
Gregory Eales <gregoeales@aol.com>
2019-10-29 13:47:25 -0700

    Update data_retreiver.py

--- utils/data_retreiver.py
+++ utils/data_retreiver.py
@@ -2,11 +2,17 @@ import numpy as np
 import torch
 
 import selenium
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 import pandas as pd
 
 
 class DataRetreiver(object):
 
     def __init__(self):
+        pass
+
+    def check_connection(self):
+        pass
+
+    def get_sentiment(self, ticker):
         pass
